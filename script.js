cacd0d3aea0611ac95f063b92e99b2d73a1ab639
Davron <davron1104@yandex.com>
2023-01-03 16:57:59 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const box = document.querySelector('.box');
+
+const newHeight = 0;
+const newWidth = 400;
+
+function changeParams(elem, h, w) {
+    elem.style.height = `${h ?? 200}px`;
+    elem.style.width = `${w ?? 200}px`;
+}
+
+changeParams(box, newHeight, newWidth);
+
+let userName;
+console.log(userName ?? 'User');
